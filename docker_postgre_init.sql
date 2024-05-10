380f982798c1b2511837bc3ba459ee730d39152e
Vijay <vijaybhaskarc@vijayc-mac-2023.local>
2024-05-10 14:45:03 +0530

    update

---
+++ docker_postgre_init.sql
@@ -0,0 +1,10 @@
+CREATE USER test WITH PASSWORD 'test';
+
+CREATE DATABASE postgres
+WITH
+OWNER = test
+ENCODING = 'UTF8'
+LC_COLLATE = 'en_US.utf8'
+LC_C_TYPE = 'en_US.utf8'
+TABLESPACE = pg_default
+CONNECTION LIMIT = -1;
